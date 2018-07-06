9712e5020d7222d0c2665a202cd88920986f6f1d
Coby Benveniste <Coby.Benveniste@gmail.com>
2018-07-06 15:08:14 +0300

    Added the blog folder as a temporary holder to contain the blog code so I can edit the theme to my liking

---
+++ blog-temp/themes/default/footer.php
@@ -0,0 +1,19 @@
+		<div class="wrap">
+	            <footer id="bottom">
+	                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
+
+	                <ul role="navigation">
+	                    <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
+
+	                    <li><a href="<?php echo base_url('admin'); ?>" title="Administer your site!">Admin area</a></li>
+
+                        <li><a href="<?php echo base_url(); ?>" title="Return to my blog home page.">Blog Home</a></li>
+
+                        <li><a href="/" title="Return to my home page.">Home</a></li>
+	                </ul>
+	            </footer>
+
+	        </div>
+        </div>
+    </body>
+</html>
